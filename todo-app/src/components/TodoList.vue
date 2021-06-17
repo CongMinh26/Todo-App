5a36acb76f1d4e662c764c6f2d1b4721e9ef86ac
CongMinh26 <congminhad1999@gmail.com>
2021-06-17 19:05:44 +0700

    commit initial
    todo

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</h3>
+  
+    <h3>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</h3>
+
+    <todo  v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" v-bind:todo="todo" :key="todo"></todo>
+
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods:{
+    deleteTodo(todo){
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex,1)
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  }
+};
+</script>
+<style >
+
+</style>
